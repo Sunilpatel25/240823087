36961e86213163ed10432bb6fb135a1b8576bbbe
Sunilpatel25 <savaliyasunil25@gmail.com>
2025-07-15 11:02:58 +0530

    javascript basic pg

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log("sunil savaliya")
+
+// function add(a,b) {
+//      sum=a+b;
+// }
+
+// 1.1. Check Even or Odd
+// Definition: Determine if a number is even or odd.
+
+// const number=7;
+
+// if(number % 2 == 0){
+//      console.log("Even");
+// }
+// else 
+// {
+//     console.log("odd");
+// }
+
+
+// 2. Find the Maximum of Two Numbers
+
+const a=5;
+const b=9;
+
+if(a>b){
+    console.log(`${a}`);
+}
+else
+{
+    console.log(`${b}`);
+}
